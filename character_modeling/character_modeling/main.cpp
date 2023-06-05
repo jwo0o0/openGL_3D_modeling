a7f3e01d84d2686faee95746bb7e0f0b1964d056
jwo0o0 <jwo0o0358@gmail.com>
2023-06-05 21:33:08 +0900

    텍스처 이미지 수정

--- character_modeling/character_modeling/main.cpp
+++ character_modeling/character_modeling/main.cpp
@@ -439,7 +439,7 @@ int main (int argc, char** argv)
     glutInit (&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
     glutInitWindowPosition (100, 100);
-    glutInitWindowSize (1000, 1000);
+    glutInitWindowSize (800, 800);
     glutCreateWindow ("Miffy");
     init();
     InitLight();
